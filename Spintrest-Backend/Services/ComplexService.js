bf5fffee17032f5cc9dae427689a1560e8674001
Kat <Katlego.Kungoane@bbd.co.za>
2024-05-02 21:43:58 +0200

    SPIN-28: complete and tested

--- Spintrest-Backend/Services/ComplexService.js
+++ Spintrest-Backend/Services/ComplexService.js
@@ -8,7 +8,30 @@ const getUsersWhoLikedPin = async (pinID) => {
     return await complexRepository.getUsersWhoLikedPin(pinID);
 }
 
+const getCommentsForPin = async (pinID) => {
+    return await complexRepository.getCommentsForPin(pinID);
+}
+
+const getPinsForBoard = async (
+    boardID,
+    isLimited = false
+) => {
+    return await complexRepository.getPinsForBoard(boardID, isLimited);
+}
+
+const getNumberOfPinsInBoard = async (boardID) => {
+    return await complexRepository.getNumberOfPinsInBoard(boardID);
+}
+
+const getBoardsForUser = async (googleUserID) => {
+    return await complexRepository.getBoardsForUser(googleUserID);
+}
+
 module.exports = {
-    getUsersWhoLikedComment,
-    getUsersWhoLikedPin
+    getPinsForBoard,
+    getBoardsForUser,
+    getCommentsForPin,
+    getUsersWhoLikedPin,
+    getNumberOfPinsInBoard,
+    getUsersWhoLikedComment
 };
