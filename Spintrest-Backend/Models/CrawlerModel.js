0d496b48a43254df48f948d2f13db09a00fce0bf
Kat <Katlego.Kungoane@bbd.co.za>
2024-05-03 18:22:22 +0200

    spin-31: added the extra columns back

--- Spintrest-Backend/Models/CrawlerModel.js
+++ Spintrest-Backend/Models/CrawlerModel.js
@@ -4,6 +4,7 @@ const mapCrawler = (crawler) => {
     return {
         crawlerID: crawler.crawlerid !== undefined ? crawler.crawlerid : crawler.crawlerID,
         crawlerEmail: crawler.crawleremail !== undefined ? crawler.crawleremail : crawler.crawlerEmail,
+        crawlerUserName: crawler.crawlerusername !== undefined ? crawler.crawlerusername : crawler.crawlerUserName,
         crawlerIsDeleted: crawler.crawlerisdeleted !== undefined ? crawler.crawlerisdeleted : crawler.crawlerIsDeleted
     }
 }
--- Spintrest-Backend/Models/WebModel.js
+++ Spintrest-Backend/Models/WebModel.js
@@ -4,6 +4,7 @@ const mapWeb = (web) => {
     return {
         webID: web.webid !== undefined ? web.webid : web.webID,
         crawlerID: web.crawlerid !== undefined ? web.crawlerid : web.crawlerID,
+        webDescription: web.webdescription !== undefined ? web.webdescription : web.webDescription,
         webIsDeleted: web.webisdeleted !== undefined ? web.webisdeleted : web.webIsDeleted,
         webTitle: web.webtitle !== undefined ? web.webtitle : web.webTitle
     }
