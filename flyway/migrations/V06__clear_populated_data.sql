37d145411e28c7390aecad443b46d9b9773d2c71
Cameron <cameron.zwane@bbd.co.za>
2024-05-09 17:18:17 +0200

    clear db

---
+++ flyway/migrations/V06__clear_populated_data.sql
@@ -0,0 +1,9 @@
+delete from CommentLikes;
+delete from SpinLikes;
+delete from SpinTags;
+delete from Tag;
+delete from WebSpins;
+delete from SpinComment;
+delete from Web;
+delete from Spin;
+delete from Crawler;
